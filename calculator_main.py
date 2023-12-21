bb717e95d81fb08472364e6d3cbb42c9a5f4d007
creche9906 <creche415@naver.com>
2023-12-21 23:02:13 +0900

    issue 5 complete

--- calculator_main.py
+++ calculator_main.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from PyQt5.QtWidgets import *
 
 class Main(QDialog):
@@ -10,30 +11,18 @@ class Main(QDialog):
         main_layout = QVBoxLayout()
         layout_equation_solution = QFormLayout()
 
-# 수식 입력과 답 출력을 위한 라벨 생성
+#
         label_equation = QLabel("")
         self.equation_solution = QLineEdit("")
-        layout_equation_solution.addRow(label_equation, self.equation_solution)
 
-#layout 생성
+#레이아웃 추가
+        layout_equation_solution.addRow(label_equation, self.equation_solution)
         layout_number = QGridLayout()
         layout_funtion = QGridLayout()
 
+        
 
-# 버튼 생성
-        button_division = QPushButton("÷")
-        button_multiply = QPushButton("x")
-        button_minus = QPushButton("-")
-        button_plus = QPushButton("+")
-        button_equal = QPushButton("=")
-        button_clear = QPushButton("C")  
-        button_clearE = QPushButton("CE")
-        button_backspace = QPushButton("<-")  
-        button_remainder = QPushButton("%")
-        button_reciprocal = QPushButton("1/x")
-        button_root = QPushButton("√")
-        button_square = QPushButton("x^2")
-
+#버튼 추가
         number_button_dict = {}
         for row in range(4):
             for col in range(3):
@@ -50,10 +39,39 @@ class Main(QDialog):
         button_dot.clicked.connect(lambda state, num=".": self.number_button_clicked(num))
         button_reverse.clicked.connect(lambda state, num="-": self.number_button_clicked(num))
 
- # 버튼 레이아웃 추가
+
         layout_number.addWidget(button_reverse, 3, 0)
         layout_number.addWidget(button_zero, 3, 1)
         layout_number.addWidget(button_dot, 3, 2)
+        button_division = QPushButton("÷")
+        button_multiply = QPushButton("x")
+        button_minus = QPushButton("-")
+        button_plus = QPushButton("+")
+        button_equal = QPushButton("=")
+        button_clear = QPushButton("C")  
+        button_clearE = QPushButton("CE")
+        button_backspace = QPushButton("<-")  
+        button_remainder = QPushButton("%")
+        button_reciprocal = QPushButton("1/x")
+        button_square = QPushButton("x^2")
+        button_root = QPushButton("√")
+        
+
+ # funtion 버튼들 시그널 설정
+        button_division.clicked.connect(lambda state, operation="/": self.button_operation_clicked(operation))
+        button_multiply.clicked.connect(lambda state, operation="*": self.button_operation_clicked(operation))
+        button_minus.clicked.connect(lambda state, operation="-": self.button_operation_clicked(operation))
+        button_plus.clicked.connect(lambda state, operation="+": self.button_operation_clicked(operation))
+        button_equal.clicked.connect(self.button_equal_clicked)
+        button_clear.clicked.connect(self.button_clear_clicked)
+        button_clearE.clicked.connect(self.button_clearE_clicked)
+        button_backspace.clicked.connect(self.button_backspace_clicked)
+        button_remainder.clicked.connect(lambda state, operation="%": self.button_operation_clicked(operation))
+        button_reciprocal.clicked.connect(self.button_reciprocal_clicked)
+        button_root.clicked.connect(self.button_root_clicked)
+        button_square.clicked.connect(self.button_square_clicked)
+
+# 레이아웃 추가
         layout_funtion.addWidget(button_division, 1, 3)
         layout_number.addWidget(button_multiply, 0, 3)
         layout_number.addWidget(button_minus, 1, 3)
@@ -67,17 +85,6 @@ class Main(QDialog):
         layout_funtion.addWidget(button_root, 1, 2)
         layout_number.addWidget(button_equal, 3, 3)
 
- # 버튼 눌렀을 때 기능 설정
-        button_division.clicked.connect(lambda state, operation="/": self.button_operation_clicked(operation))
-        button_multiply.clicked.connect(lambda state, operation="*": self.button_operation_clicked(operation))
-        button_minus.clicked.connect(lambda state, operation="-": self.button_operation_clicked(operation))
-        button_plus.clicked.connect(lambda state, operation="+": self.button_operation_clicked(operation))
-        button_equal.clicked.connect(self.button_equal_clicked)
-        button_clear.clicked.connect(self.button_clear_clicked)
-        button_clearE.clicked.connect(self.button_clearE_clicked)
-        button_backspace.clicked.connect(self.button_backspace_clicked)
-        button_remainder.clicked.connect(lambda state, operation="%": self.button_operation_clicked(operation))
-
 # 각 레이아웃을 main_layout 레이아웃에 추가
         main_layout.addWidget(self.equation_solution)
         main_layout.addLayout(layout_funtion)
@@ -86,8 +93,7 @@ class Main(QDialog):
         self.show()
 
     #################
-# 계산기 기능에 관한 함수들 정의
-        
+#기능 함수 정의
     def number_button_clicked(self, num):
         equation = self.equation_solution.text()
         equation += str(num)
@@ -97,7 +103,7 @@ class Main(QDialog):
         equation = self.equation_solution.text()
         equation += operation
         self.equation_solution.setText(equation)
-
+    
     def button_equal_clicked(self):
         equation = self.equation_solution.text()
         try:
@@ -116,7 +122,29 @@ class Main(QDialog):
         equation = self.equation_solution.text()
         equation = equation[:-1]
         self.equation_solution.setText(equation)
-        
+    def button_reciprocal_clicked(self):
+        equation = self.equation_solution.text()
+        try:
+            reciprocal = str(1 / eval(equation))
+            self.equation_solution.setText(reciprocal)
+        except Exception as e:
+            self.equation_solution.setText("Error")
+
+    def button_root_clicked(self):
+        equation = self.equation_solution.text()
+        try:
+            square_root = str(math.sqrt(eval(equation)))
+            self.equation_solution.setText(square_root)
+        except Exception as e:
+            self.equation_solution.setText("Error")
+    def button_square_clicked(self):
+        equation = self.equation_solution.text()
+        try:
+            square = str(eval(equation) ** 2)
+            self.equation_solution.setText(square)
+        except Exception as e:
+            self.equation_solution.setText("Error")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
