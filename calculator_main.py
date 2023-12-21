d14a9761d8512e2080ec00ba194fdba2dadce55c
creche9906 <creche415@naver.com>
2023-12-21 22:08:27 +0900

    issue 3 complete

--- calculator_main.py
+++ calculator_main.py
@@ -18,8 +18,9 @@ class Main(QDialog):
         layout_equation_solution.addRow(label_equation, self.equation_solution)
 
 
-        # 숫자 버튼 생성하고, layout_number 레이아웃에 추가
         layout_number = QGridLayout()
+        layout_funtion = QGridLayout()
+
         number_button_dict = {}
         for row in range(4):
             for col in range(3):
@@ -29,16 +30,15 @@ class Main(QDialog):
                                                             self.number_button_clicked(num))
                 layout_number.addWidget(number_button_dict[number], row, col)
 
-        # 0 버튼, 소숫점 버튼, 00 버튼 추가
         button_zero = QPushButton("0")
         button_dot = QPushButton(".")
-        button_double_zero = QPushButton("00")
+        button_reverse = QPushButton("+/-")
         button_zero.clicked.connect(lambda state, num="0": self.number_button_clicked(num))
         button_dot.clicked.connect(lambda state, num=".": self.number_button_clicked(num))
-        button_double_zero.clicked.connect(lambda state, num="00": self.number_button_clicked(num))
+        button_reverse.clicked.connect(lambda state, num="-": self.number_button_clicked(num))
 
         # 숫자 및 소숫점 버튼 추가
-        layout_number.addWidget(button_double_zero, 3, 0)
+        layout_number.addWidget(button_reverse, 3, 0)
         layout_number.addWidget(button_zero, 3, 1)
         layout_number.addWidget(button_dot, 3, 2)
 
@@ -55,33 +55,42 @@ class Main(QDialog):
         button_plus.clicked.connect(lambda state, operation="+": self.button_operation_clicked(operation))
 
         # 사칙연산 버튼 추가
-        layout_number.addWidget(button_division, 0, 3)
-        layout_number.addWidget(button_multiply, 1, 3)
-        layout_number.addWidget(button_minus, 2, 3)
-        layout_number.addWidget(button_plus, 3, 3)
+        layout_funtion.addWidget(button_division, 1, 3)
+        layout_number.addWidget(button_multiply, 0, 3)
+        layout_number.addWidget(button_minus, 1, 3)
+        layout_number.addWidget(button_plus, 2, 3)
 
-        # =, clear, backspace 버튼 생성
+        # funtion에 있는 button추가
         button_equal = QPushButton("=")
-        button_clear = QPushButton("C")  # Use "C" for clear instead of "Clear"
+        button_clear = QPushButton("C")  
         button_clearE = QPushButton("CE")
-        button_backspace = QPushButton("<-")  # Use "<-" for backspace
+        button_backspace = QPushButton("<-")  
+        button_remainder = QPushButton("%")
+        button_reciprocal = QPushButton("1/x")
+        button_root = QPushButton("√")
+        button_square = QPushButton("x^2")
 
-        # =, clear, backspace 버튼 클릭 시 시그널 설정
+        # funtion 버튼들 시그널 설정
         button_equal.clicked.connect(self.button_equal_clicked)
         button_clear.clicked.connect(self.button_clear_clicked)
         button_clearE.clicked.connect(self.button_clearE_clicked)
         button_backspace.clicked.connect(self.button_backspace_clicked)
+        button_remainder.clicked.connect(lambda state, operation="%": self.button_operation_clicked(operation))
 
         # =, clear, backspace 버튼을 layout_clear_equal 레이아웃에 추가
-        layout_clear_equal = QGridLayout()
-        layout_clear_equal.addWidget(button_backspace, 0, 3)
-        layout_clear_equal.addWidget(button_clear, 0, 1)
-        layout_clear_equal.addWidget(button_clearE, 0, 2)
-        layout_clear_equal.addWidget(button_equal, 0, 0)
+
+        layout_funtion.addWidget(button_remainder, 0, 0)
+        layout_funtion.addWidget(button_clearE, 0, 1)
+        layout_funtion.addWidget(button_clear, 0, 2)
+        layout_funtion.addWidget(button_backspace, 0, 3)
+        layout_funtion.addWidget(button_reciprocal, 1, 0)
+        layout_funtion.addWidget(button_square, 1, 1)
+        layout_funtion.addWidget(button_root, 1, 2)
+        layout_number.addWidget(button_equal, 3, 3)
 
         # 각 레이아웃을 main_layout 레이아웃에 추가
         main_layout.addWidget(self.equation_solution)
-        main_layout.addLayout(layout_clear_equal)
+        main_layout.addLayout(layout_funtion)
         main_layout.addLayout(layout_number)
         self.setLayout(main_layout)
         self.show()
@@ -117,6 +126,7 @@ class Main(QDialog):
         equation = self.equation_solution.text()
         equation = equation[:-1]
         self.equation_solution.setText(equation)
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
