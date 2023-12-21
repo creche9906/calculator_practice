f4b3c04961e03af482afe3f6dc43efe0344b86e2
creche9906 <creche415@naver.com>
2023-12-21 20:16:15 +0900

    issue 1 complete

--- calculator_main.py
+++ calculator_main.py
@@ -16,26 +16,23 @@ class Main(QDialog):
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Solution: ")
-        self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
+        label_equation = QLabel("")
+        self.equation_solution = QLineEdit("")  # Combined widget for equation and solution
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addRow(label_equation, self.equation)
-        layout_equation_solution.addRow(label_solution, self.solution)
+        layout_equation_solution.addRow(label_equation, self.equation_solution)
 
-        ### 사칙연상 버튼 생성
+        ### 사칙연산 버튼 생성
         button_plus = QPushButton("+")
         button_minus = QPushButton("-")
         button_product = QPushButton("x")
         button_division = QPushButton("/")
 
         ### 사칙연산 버튼을 클릭했을 때, 각 사칙연산 부호가 수식창에 추가될 수 있도록 시그널 설정
-        button_plus.clicked.connect(lambda state, operation = "+": self.button_operation_clicked(operation))
-        button_minus.clicked.connect(lambda state, operation = "-": self.button_operation_clicked(operation))
-        button_product.clicked.connect(lambda state, operation = "*": self.button_operation_clicked(operation))
-        button_division.clicked.connect(lambda state, operation = "/": self.button_operation_clicked(operation))
+        button_plus.clicked.connect(lambda state, operation="+": self.button_operation_clicked(operation))
+        button_minus.clicked.connect(lambda state, operation="-": self.button_operation_clicked(operation))
+        button_product.clicked.connect(lambda state, operation="*": self.button_operation_clicked(operation))
+        button_division.clicked.connect(lambda state, operation="/": self.button_operation_clicked(operation))
 
         ### 사칙연산 버튼을 layout_operation 레이아웃에 추가
         layout_operation.addWidget(button_plus)
@@ -63,21 +60,21 @@ class Main(QDialog):
         number_button_dict = {}
         for number in range(0, 10):
             number_button_dict[number] = QPushButton(str(number))
-            number_button_dict[number].clicked.connect(lambda state, num = number:
+            number_button_dict[number].clicked.connect(lambda state, num=number:
                                                        self.number_button_clicked(num))
-            if number >0:
-                x,y = divmod(number-1, 3)
+            if number > 0:
+                x, y = divmod(number - 1, 3)
                 layout_number.addWidget(number_button_dict[number], x, y)
-            elif number==0:
+            elif number == 0:
                 layout_number.addWidget(number_button_dict[number], 3, 1)
 
         ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
-        button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
+        button_dot.clicked.connect(lambda state, num=".": self.number_button_clicked(num))
         layout_number.addWidget(button_dot, 3, 2)
 
         button_double_zero = QPushButton("00")
-        button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
+        button_double_zero.clicked.connect(lambda state, num="00": self.number_button_clicked(num))
         layout_number.addWidget(button_double_zero, 3, 0)
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가
@@ -93,28 +90,30 @@ class Main(QDialog):
     ### functions ###
     #################
     def number_button_clicked(self, num):
-        equation = self.equation.text()
+        equation = self.equation_solution.text()
         equation += str(num)
-        self.equation.setText(equation)
+        self.equation_solution.setText(equation)
 
     def button_operation_clicked(self, operation):
-        equation = self.equation.text()
+        equation = self.equation_solution.text()
         equation += operation
-        self.equation.setText(equation)
+        self.equation_solution.setText(equation)
 
     def button_equal_clicked(self):
-        equation = self.equation.text()
-        solution = eval(equation)
-        self.solution.setText(str(solution))
+        equation = self.equation_solution.text()
+        try:
+            solution = str(eval(equation))
+            self.equation_solution.setText(solution)
+        except Exception as e:
+            self.equation_solution.setText("Error")
 
     def button_clear_clicked(self):
-        self.equation.setText("")
-        self.solution.setText("")
+        self.equation_solution.setText("")
 
     def button_backspace_clicked(self):
-        equation = self.equation.text()
+        equation = self.equation_solution.text()
         equation = equation[:-1]
-        self.equation.setText(equation)
+        self.equation_solution.setText(equation)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
